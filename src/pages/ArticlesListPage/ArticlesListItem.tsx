22552fdb28fab8853d5fd13df614b0ea211324da
Mateusz Pokora <vrael560@gmail.com>
2019-09-29 19:49:54 +0200

    add ArticlesListItem placeholder values

--- src/pages/ArticlesListPage/ArticlesListItem.tsx
+++ src/pages/ArticlesListPage/ArticlesListItem.tsx
@@ -8,7 +8,19 @@ interface ArticleListItemProps {
 const ArticleListItem = (props: ArticleListItemProps) => {
   const { article } = props;
 
-  return <div>{article.title}</div>;
+  return (
+    <div>
+      <img src={article.urlToImage} alt="article-image" width="100px" />
+      <div>
+        <span>{article.publishedAt}</span>
+        <span>{article.author}</span>
+        <span>{article.source.name}</span>
+      </div>
+      <h3>{article.title}</h3>
+      <p>{article.description}</p>
+      <button>read more</button>
+    </div>
+  );
 };
 
 export default ArticleListItem;
